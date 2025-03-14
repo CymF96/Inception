969e7bc6cf5b8e274b0d93b043c8f510a6ae01bb
cofische <cofische@student.42london.com>
2025-03-14 13:23:54 +0000

    testing pwd

--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -11,7 +11,7 @@ GRANT SELECT, INSERT, UPDATE ON *.* TO 'RandomUser'@'localhost' IDENTIFIED BY 'D
 CREATE DATABASE Travel;
 USE Travel;
 CREATE TABLE Visited_countries (country VARCHAR(50), nb_visits INT, last_visited DATE);
-INSERT INTO Visited_countries (country, nb_visits, last_visisted) VALUES ('France', 5, '2024-12-25'), ('Japan', 1, '2019-09-15'), ('USA', 3, '2016-07-01');
+INSERT INTO Visited_countries (country, nb_visits, last_visited) VALUES ('France', 5, '2024-12-25'), ('Japan', 1, '2019-09-15'), ('USA', 3, '2016-07-01');
 
 -- exit mariadb
 FLUSH PRIVILEGES;
