0c52e67bb1bbdfb9d1897bd16bf622875b8f8207
CymF96 <cym.fischer@gmail.com>
2025-03-17 17:24:34 +0000

    fixing Docker pb for mariadb service

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -22,7 +22,7 @@ else
 	CREATE TABLE Visited_countries (country VARCHAR(50), nb_visits INT, last_visited DATE);
 	INSERT INTO Visited_countries (country, nb_visits, last_visited) VALUES ('France', 5, '2024-12-25'), ('Japan', 1, '2019-09-15'), ('USA', 3, '2016-07-01');
 	FLUSH PRIVILEGES;
-	EXIT;
+	EXIT
 EOF
 
 	echo "Database setup completed."
