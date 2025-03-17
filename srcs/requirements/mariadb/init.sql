f3f4cc79c06cc96a1e4609cfa2cd871f5cf73089
CymF96 <cym.fischer@gmail.com>
2025-03-17 15:06:49 +0000

    fixing Docker pb for mariadb service

--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,12 +1,3 @@
--- Secure Installation of Mariadb with removing unecessary test elements
---DELETE FROM mysql.user WHERE User='';
---DROP DATABASE IF EXISTS test;
-
--- Creating 2 users: Administrator and RandomUser
---GRANT ALL ON *.* TO '$DB_ADMIN_ID'@'localhost' IDENTIFIED BY '$DB_ADMIN_PWD' WITH GRANT OPTION;
--- UPDATE mysql.user SET Host='localhost' WHERE User='$DB_ADMIN_ID';
---GRANT SELECT, INSERT, UPDATE ON *.* TO 'RandomUser'@'localhost' IDENTIFIED BY 'D@ckerUzeR2025+';
-
 -- Create Example Data
 CREATE DATABASE Travel;
 USE Travel;
@@ -14,5 +5,4 @@ CREATE TABLE Visited_countries (country VARCHAR(50), nb_visits INT, last_visited
 INSERT INTO Visited_countries (country, nb_visits, last_visited) VALUES ('France', 5, '2024-12-25'), ('Japan', 1, '2019-09-15'), ('USA', 3, '2016-07-01');
 
 -- exit mariadb
-FLUSH PRIVILEGES;
 EXIT
--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 set -e  # Exit on error
 
-# Load secrets if they exist
-#if [ -f "/run/secrets/db_admin_pwd" ]; then
-#	DB_ADMIN_PWD=$(cat /run/secrets/db_admin_pwd)
-#fi
-#if [ -f "/run/secrets/db_pwd" ]; then
-#	DB_PWD=$(cat /run/secrets/db_pwd)
-#fi
-
-# Debugging output
-echo "DB_ADMIN_ID: $DB_ADMIN_ID"
-echo "DATABASE: $DATABASE"
-echo "DB_ADMIN_PWD: [HIDDEN]"
-echo "DB_PWD: [HIDDEN]"
-
 # Start MariaDB service
 service mariadb start
 
 # Check if database exists
 if [ -d "/var/lib/mysql/$DATABASE" ]; then 
-	echo "Database already exists"
+	echo "Database already exists, starting service only"
 else
 	echo "Initializing database..."
 
 	# Execute SQL commands
 	mariadb -u root <<EOF
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-GRANT ALL ON *.* TO '$DB_ADMIN_ID'@'localhost' IDENTIFIED BY '$DB_ADMIN_PWD' WITH GRANT OPTION;
-GRANT SELECT, INSERT, UPDATE ON *.* TO '$DB_ID'@'localhost' IDENTIFIED BY '$DB_PWD';
-FLUSH PRIVILEGES;
+	DELETE FROM mysql.user WHERE User='';
+	DROP DATABASE IF EXISTS test;
+	GRANT ALL ON *.* TO '$DB_ADMIN_ID'@'localhost' IDENTIFIED BY '$DB_ADMIN_PWD' WITH GRANT OPTION;
+	GRANT SELECT, INSERT, UPDATE ON *.* TO '$DB_ID'@'localhost' IDENTIFIED BY '$DB_PWD';
+	FLUSH PRIVILEGES;
 EOF
 
-	# Execute additional initialization SQL file
-	mariadb -u root < /usr/local/bin/init.sql
+	#Execute additional initialization SQL file
+	mariadb -u $DB_ADMIN_ID -p $DB_ADMIN_PWD < /usr/local/bin/init.sql
 
 	echo "Database setup completed."
 fi
