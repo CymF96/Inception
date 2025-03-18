22485b4963a252106d4a5f52e89ffc4f3b251734
CymF96 <cym.fischer@gmail.com>
2025-03-18 15:37:06 +0000

    fixing wordpress container

--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -11,7 +11,7 @@
 if [ ! -f /var/www/cofische/wp-config.php ]; then
 
 	#move custom wp-config.php to /var/www/cofische + give permission and ownership to wd-php
-	mv /tmp/www.conf /etc/php/8.2/fpm/pool.d/www.conf
+	#mv /tmp/www.conf /etc/php/8.2/fpm/pool.d/www.conf
 	cat /etc/php/8.2/fpm/pool.d/www.conf
 	mv /tmp/wp-config.php /var/www/cofische/wp-config.php
 	chown -R www-data:www-data /var/www/cofische/wp-config.php && chmod +x /var/www/cofische/wp-config.php
