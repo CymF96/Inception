34f5d17bfa35a5c808dd77dbe7d537b683325124
CymF96 <cym.fischer@gmail.com>
2025-03-18 09:34:07 +0000

    fixing wordpress container

--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -5,14 +5,14 @@ if [ ! -f /var/www/cofische/wp-config.php ]; then
 
 else
 
-	if [ ! wp core is-installed ]; then
-		wp core install \
-			--url="$SITE_URL"
+	if ! wp core is-installed; then
+		wp core install --allow-root \
+			--url="$SITE_URL" \
 			--title="$SITE_TITLE" \
 			--admin_user="$WP_ADMIN_USER" \
 			--admin_password="$WP_ADMIN_PASSWORD" \
 			--admin_email="$WP_ADMIN_EMAIL" \
-			--allow-root
+			
 	fi
 
 	echo "wordpress installation completed"
