1d86125f37ce3da9918435a7545dfe11e467048f
CymF96 <cym.fischer@gmail.com>
2025-03-18 15:50:40 +0000

    fixing wordpress container

--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-#if pgrep -x "php-fpm8.2" > /dev/null; then
-#    echo "Stopping existing PHP-FPM service..."
-#    service php8.2-fpm stop
-#    rm -rf /run/php/php8.2-fpm.sock  # Remove the stale socket file
-#fi
-
-#service php8.2-fpm start
+cat /etc/php/8.2/fpm/pool.d/www.conf
 
 if [ ! -f /var/www/cofische/wp-config.php ]; then
 
 	#move custom wp-config.php to /var/www/cofische + give permission and ownership to wd-php
 	#mv /tmp/www.conf /etc/php/8.2/fpm/pool.d/www.conf
-	cat /etc/php/8.2/fpm/pool.d/www.conf
 	mv /tmp/wp-config.php /var/www/cofische/wp-config.php
 	chown -R www-data:www-data /var/www/cofische/wp-config.php && chmod +x /var/www/cofische/wp-config.php
 	cd /var/www/cofische || { echo "Failed to change directory!"; exit 1; }
