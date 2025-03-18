afcc8132eb2dfe7a5eadec766d6116dfa4f6669b
CymF96 <cym.fischer@gmail.com>
2025-03-18 11:43:24 +0000

    fixing wordpress container

--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -15,16 +15,16 @@ else
 	cd /var/www/cofische || { echo "Failed to change directory!"; exit 1; }
 
 	#wait for correct move and setup wordpress
-	sleep 2
-	if ! wp core is-installed; then
-		wp core install --allow-root \
-			--url="$SITE_URL" \
-			--title="$SITE_TITLE" \
-			--admin_user="$WP_ADMIN_USER" \
-			--admin_password="$WP_ADMIN_PASSWORD" \
-			--admin_email="$WP_ADMIN_EMAIL" \
+	#sleep 2
+	#if ! wp core is-installed; then
+	#	wp core install --allow-root \
+	#		--url="$SITE_URL" \
+	#		--title="$SITE_TITLE" \
+	#		--admin_user="$WP_ADMIN_USER" \
+	#		--admin_password="$WP_ADMIN_PASSWORD" \
+	#		--admin_email="$WP_ADMIN_EMAIL" \
 		
-	fi
+	#fi
 
 	echo "wordpress installation completed"
 
