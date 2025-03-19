a47689c2ea133054575b4c2c3c30066a588b833c
CymF96 <cym.fischer@gmail.com>
2025-03-19 17:42:25 +0000

    correction of Inception project

--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-sleep 5
-if [ ! -f /var/www/cofische/wp-config.php ]; then
-	wp config create --allow-root  --skip-check\
-	--dbname=$DATABASE \
-	--dbuser=$DB_ADMIN_IDR \
-	--dbpass=$BD_ADMIN_PWD \
-	--dbhost=$DB_HOST 
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
 
-	echo "WordPress installation completed"
-	sleep 5
-	cd /var/www/cofische
-	
+####### MANDATORY PART ##########
+
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+
+	#Inport env variables in the config file
+	sed -i "s/username_here/$DB_ADMIN_ID/g" wp-config-sample.php
+	sed -i "s/password_here/$DB_ADMIN_PWD/g" wp-config-sample.php
+	sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MDATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+
 	wp core install --allow-root --path=/var/www/cofische \
 		--url="$SITE_URL" \
 		--title="$SITE_TITLE" \
