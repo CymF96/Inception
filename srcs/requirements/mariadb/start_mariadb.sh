4a4edbb4c0301c38ff961ba227d5c24ed1c64f9f
cofische <cofische@student.42london.com>
2025-03-20 13:25:42 +0000

    testing

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,23 +2,23 @@
 set -e  # Exit on error
 
 # Start MariaDB service
-service mysql start
+service mariadb start
 sleep 10
 
 # Create database Travel if not exists
 echo "Creating new database $DATABASE"
-mysql -e "CREATE DATABASE IF NOT EXISTS $DATABASE;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS $DATABASE;"
 
 # Create the table Visited_countries
 echo "Creating elements inside $DATABASE"
-mysql -e "USE $DATABASE; CREATE TABLE IF NOT EXISTS Visited_countries (
+mariadb -e "USE $DATABASE; CREATE TABLE IF NOT EXISTS Visited_countries (
     country VARCHAR(50),
     nb_visits INT,
     last_visited DATE
 );"
 
 # Insert initial data into Visited_countries table
-mysql -e "USE $DATABASE; INSERT INTO Visited_countries (country, nb_visits, last_visited) 
+mariadb -e "USE $DATABASE; INSERT INTO Visited_countries (country, nb_visits, last_visited) 
 VALUES 
     ('France', 5, '2024-12-25'),
     ('Japan', 1, '2019-09-15'),
@@ -26,14 +26,14 @@ VALUES
 
 # Create admin user with root privileges
 echo "Creating new users $DB_ADMIN_ID && $DB_ID"
-mysql -e "CREATE USER IF NOT EXISTS '${DB_ID}'@'%' IDENTIFIED BY '${DB_PWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_ID}'@'%' WITH GRANT OPTION;"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${DB_ROOT_PWD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
-mysqladmin -u root -p$DB_ROOT_PWD shutdown
+mariadb -e "CREATE USER IF NOT EXISTS '${DB_ID}'@'%' IDENTIFIED BY '${DB_PWD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_ID}'@'%' WITH GRANT OPTION;"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${DB_ROOT_PWD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
+mariadbadmin -u root -p$DB_ROOT_PWD shutdown
 
-echo "Users created successfully and MySQL service restart"
+echo "Users created successfully and mariadb service restart"
 
 # Executing CMD from Dockerfile after ending the script
 exec "$@"
