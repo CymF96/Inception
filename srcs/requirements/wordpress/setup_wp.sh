a1f99100a7dbfe29299bf9e6a556dbdfbdf8460d
CymF96 <cym.fischer@gmail.com>
2025-03-21 10:38:03 +0000

    fixing wp issue

--- srcs/requirements/wordpress/setup_wp.sh
+++ srcs/requirements/wordpress/setup_wp.sh
@@ -40,12 +40,12 @@ else
 	line8=$(sed -n '8p' /tmp/wp-keys.txt)
 	awk -v line="$line1" '/\'AUTH_KEY\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
 	#awk -v line="$line2" '/\'SECURE_AUTH_KEY\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
-	sed -i '52p' $line2 $WP_PATH/wp-config.php
+	sed -i "52c\\$line2" $WP_PATH/wp-config.php
 	awk -v line="$line3" '/\'LOGGED_IN_KEY\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
 	awk -v line="$line4" '/\'NONCE_KEY\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
 	awk -v line="$line5" '/\'AUTH_SALT\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
 	#awk -v line="$line6" '/\'SECURE_AUTH_SALT\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
-	sed -i '56p' $line2 $WP_PATH/wp-config.php
+	sed -i "56c\\$line2" $WP_PATH/wp-config.php
 	awk -v line="$line7" '/\'LOGGED_IN_SALT\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
 	awk -v line="$line8" '/\'_SALT\'/' {$0 = line} 1' "$WP_PATH/wp-config.php" > temp && mv temp "$WP_PATH/wp-config.php"
 	rm /tmp/wp-keys.tx
