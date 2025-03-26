b964bb4b20339e7fb22130486a1953d2529f33ad
CymF96 <cym.fischer@gmail.com>
2025-03-26 17:08:09 +0000

    adding ftp service on home testing branch

--- srcs/requirements/bonus/ftp-server/ftp_start.sh
+++ srcs/requirements/bonus/ftp-server/ftp_start.sh
@@ -2,11 +2,13 @@
 
 if [ ! -f /etc/vsftpd/vsftpd.conf ]; then
 
+	mkdir /etc/vsftpd
 	mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
 	adduser --gecos "" --disabled-password $FTP_USER
 	echo "$FTP_USER":"$FTP_USER_PWD" | chpasswd
 	mkdir -p $FTP_DIR
+	echo "ftp testing file" | tee $FTP_DIR/test.txt
 	chown -R $FTP_USER $FTP_DIR
 	echo $FTP_USER | tee -a /ect/vsftpd/vsftpd.userlist
 
